08fc7ecf4f9d5511ea33f7b7d5da987a882aefd4
mrjackmaster <jacektrg+github@gmail.com>
2022-05-06 02:26:59 +0200

    Importing components and hook module for managing dark/light mode. Adding state and function for toggling said mode. Adding data-theme property to use appropriate styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
 import './App.scss';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import useLocalStorage from 'use-local-storage';
 
 const App = () => {
-	return <div className='main'></div>;
+	const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const [theme, setTheme] = useLocalStorage<'dark' | 'light'>(
+		'theme',
+		defaultDark ? 'dark' : 'light'
+	);
+
+	const toggleTheme = () => {
+		const newTheme = theme === 'light' ? 'dark' : 'light';
+		setTheme(newTheme);
+	};
+
+	return (
+		<div id='app' className='app' data-theme={theme}>
+			<Nav theme={theme} toggleTheme={toggleTheme} />
+			<Home />
+			<About />
+			<Skills />
+			<Projects />
+			<Contact />
+		</div>
+	);
 };
 
 export default App;
