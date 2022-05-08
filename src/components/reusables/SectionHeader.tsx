71eeff9419ce6f5c15ac15c49a524391bb3ea423
mrjackmaster <jacektrg+github@gmail.com>
2022-05-08 19:14:57 +0200

    Updating to styled component and moving to subfolder.

--- src/components/reusables/SectionHeader.tsx
+++ src/components/reusables/SectionHeader.tsx
@@ -1,10 +1,12 @@
+import styles from '../../styles/SectionHeader.module.scss';
+
 interface Props {
 	text: string;
 }
 
 const SectionHeader: React.FC<Props> = ({ text }) => {
 	return (
-		<div className='section_header'>
+		<div className={styles.section_header}>
 			<hr />
 			<h2>{text}</h2>
 			<hr />
