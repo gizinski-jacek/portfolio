db4aab2c10d7d227830a152940ceab919ac9ac27
mrjackmaster <jacektrg+github@gmail.com>
2022-05-10 00:20:28 +0200

    Data about to-be rendered projects

---
+++ src/components/data/projectList.tsx
@@ -0,0 +1,74 @@
+export const projectList = [
+	{
+		name: 'Odinbook',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/odinbook-client',
+		demo: 'https://odinbook-client-97346.netlify.app/',
+	},
+	{
+		name: 'Youtube App',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/youtube-page',
+		demo: 'https://youtube-page-102934.herokuapp.com',
+	},
+	{
+		name: 'Weather App',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/Weather-App-project',
+		demo: 'https://gizinski-jacek.github.io/Weather-App-project',
+	},
+	{
+		name: 'Shopping Cart',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/shopping-cart-react',
+		demo: 'https://shopping-cart-154325.herokuapp.com',
+	},
+	{
+		name: 'CV Creator',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/cv-project-react',
+		demo: 'https://cv-project-react-543266.herokuapp.com',
+	},
+	{
+		name: 'Inventory App',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/inventory-app',
+		demo: 'https://inventory-app-568471.herokuapp.com',
+	},
+	{
+		name: 'Where is Waldo',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/where-is-waldo',
+		demo: 'https://where-is-waldo-685893.herokuapp.com',
+	},
+	{
+		name: 'Blogster',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/blog-API-MERN',
+		demo: 'https://blog-api-mern-360821.herokuapp.com',
+	},
+	{
+		name: 'ToDo List',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/ToDo-List-project',
+		demo: 'https://gizinski-jacek.github.io/ToDo-List-project',
+	},
+	{
+		name: 'Online Library',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/Library-project',
+		demo: 'https://gizinski-jacek.github.io/Library-project',
+	},
+	{
+		name: 'TNW Clone',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/responsive-design',
+		demo: 'https://responsive-design-100943.herokuapp.com',
+	},
+	{
+		name: 'Battleships',
+		preview: '',
+		github: 'https://github.com/gizinski-jacek/Battleship-game-project',
+		demo: 'https://gizinski-jacek.github.io/Battleship-game-project',
+	},
+];
