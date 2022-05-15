e1ac208bedfe99d00d993b514ddd4649e5605df6
mrjackmaster <jacektrg+github@gmail.com>
2022-05-15 02:11:34 +0200

    Adding custom properties to project items for handling animations. Adding state and functions for handling indexes of rendered projects. Applying appropriate animations using custom property.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,53 +1,149 @@
 import { useState } from 'react';
 import SectionHeader from './reusables/SectionHeader';
-import ProjectWrapper from './utils/ProjectWrapper';
 import { projectList } from './data/projectList';
-import styles from '../styles/Projects.module.scss';
+import styles from '../styles/Project.module.scss';
+
+declare module 'react' {
+	export interface HTMLAttributes<T> {
+		'data-slidebackward'?: any;
+		'data-slideforward'?: any;
+	}
+}
 
 const Projects = () => {
-	const [slideIndex, setSlideIndex] = useState(0);
-
-	const nextSlide = () => {
-		if (slideIndex > document.querySelectorAll('.project_item').length) {
-			setSlideIndex(0);
-		} else {
-			setSlideIndex((prevState) => prevState + 1);
-		}
-	};
+	const [slideBackward, setSlideBackward] = useState(0);
+	const [slideForward, setSlideForward] = useState(0);
+	const [earlierProject, setEarlierProject] = useState(projectList.length - 1);
+	const [previousProject, setPreviousProject] = useState(0);
+	const [currentProject, setCurrentProject] = useState(1);
+	const [nextProject, setNextProject] = useState(2);
+	const [laterProject, setLaterProject] = useState(3);
 
 	const previousSlide = () => {
-		if (slideIndex <= 0) {
-			setSlideIndex(document.querySelectorAll('.project_item').length);
-		} else {
-			setSlideIndex((prevState) => prevState - 1);
-		}
+		setSlideForward(1);
+		setTimeout(() => {
+			setLaterProject(nextProject);
+			setNextProject(currentProject);
+			setCurrentProject(previousProject);
+			setPreviousProject(earlierProject);
+			if (earlierProject <= 0) {
+				setEarlierProject(projectList.length - 1);
+			} else {
+				setEarlierProject((prevState) => prevState - 1);
+			}
+		}, 750);
 	};
 
-	const projectsDisplay = projectList.map((project, index) => {
-		return <ProjectWrapper key={index} data={project} />;
-	});
+	const nextSlide = () => {
+		setSlideBackward(1);
+		setTimeout(() => {
+			setEarlierProject(previousProject);
+			setPreviousProject(earlierProject);
+			setCurrentProject(nextProject);
+			setNextProject(laterProject);
+			if (laterProject >= projectList.length - 1) {
+				setLaterProject(0);
+			} else {
+				setLaterProject((prevState) => prevState + 1);
+			}
+		}, 750);
+	};
 
 	return (
 		<div id='projects' className={styles.projects}>
 			<SectionHeader text={'Featured Projects'} />
 			<div className={styles.section_projects}>
-				<div className={styles.container}>{projectsDisplay}</div>
+				<div className={styles.container}>
+					<div
+						className={`${styles.project_item} ${styles.earlierProject}`}
+						data-slideforward={slideForward}
+						onAnimationEnd={() => {
+							setSlideForward(0);
+						}}
+					>
+						<img
+							src={projectList[earlierProject].preview}
+							alt={projectList[earlierProject].name}
+						/>
+						<h3>{projectList[earlierProject].name}</h3>
+					</div>
+					<div
+						className={`${styles.project_item} ${styles.previousProject}`}
+						data-slidebackward={slideBackward}
+						data-slideforward={slideForward}
+						onAnimationEnd={() => {
+							setSlideBackward(0);
+							setSlideForward(0);
+						}}
+					>
+						<img
+							src={projectList[previousProject].preview}
+							alt={projectList[previousProject].name}
+						/>
+						<h3>{projectList[previousProject].name}</h3>
+					</div>
+					<div
+						className={`${styles.project_item} ${styles.currentProject}`}
+						data-slidebackward={slideBackward}
+						data-slideforward={slideForward}
+						onAnimationEnd={() => {
+							setSlideBackward(0);
+							setSlideForward(0);
+						}}
+					>
+						<img
+							src={projectList[currentProject].preview}
+							alt={projectList[currentProject].name}
+						/>
+						<h3>{projectList[currentProject].name}</h3>
+					</div>
+					<div
+						className={`${styles.project_item} ${styles.nextProject}`}
+						data-slidebackward={slideBackward}
+						data-slideforward={slideForward}
+						onAnimationEnd={() => {
+							setSlideBackward(0);
+							setSlideForward(0);
+						}}
+					>
+						<img
+							src={projectList[nextProject].preview}
+							alt={projectList[nextProject].name}
+						/>
+						<h3>{projectList[nextProject].name}</h3>
+					</div>
+					<div
+						className={`${styles.project_item} ${styles.laterProject}`}
+						data-slidebackward={slideBackward}
+						onAnimationEnd={() => {
+							setSlideBackward(0);
+						}}
+					>
+						<img
+							src={projectList[laterProject].preview}
+							alt={projectList[laterProject].name}
+						/>
+						<h3>{projectList[laterProject].name}</h3>
+					</div>
+				</div>
 				<div className={styles.projects_controls}>
 					<div className={styles.arrow} onClick={() => previousSlide()}>
 						&#10094;
 					</div>
 					<div className={styles.links}>
 						<a
-							href={projectList[slideIndex].github}
-							rel='noreferrer'
 							className={styles.button}
+							href={projectList[currentProject].github}
+							target='_blank'
+							rel='noreferrer'
 						>
 							Project Github
 						</a>
 						<a
-							href={projectList[slideIndex].demo}
-							rel='noreferrer'
 							className={styles.button}
+							href={projectList[currentProject].demo}
+							target='_blank'
+							rel='noreferrer'
 						>
 							Project Demo
 						</a>
