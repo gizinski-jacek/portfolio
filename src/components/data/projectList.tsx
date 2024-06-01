69628661c4aff40fa891fb471b7d414f1930d8ff
Jacek Gizinski <jacektrg+github@gmail.com>
2024-06-01 16:48:30 +0200

    Reordering

--- src/components/data/projectList.tsx
+++ src/components/data/projectList.tsx
@@ -6,11 +6,11 @@ export const projectList: {
 	demo: string;
 }[] = [
 	{
-		name: 'CV Creator',
-		thumbnail: './previews/thumbnail/preview_cv_creator_sm.png',
-		image: './previews/original/preview_cv_creator.png',
-		github: 'https://github.com/gizinski-jacek/cv-project-react',
-		demo: 'https://cv-creator-c9c2ea.netlify.app',
+		name: 'QR File Share',
+		thumbnail: './previews/thumbnail/preview_qr_file_share_sm.png',
+		image: './previews/original/preview_qr_file_share.png',
+		github: 'https://github.com/gizinski-jacek/qr-file-share',
+		demo: 'https://qr-file-share-umber.vercel.app',
 	},
 	{
 		name: 'Weather App',
@@ -18,13 +18,6 @@ export const projectList: {
 		image: './previews/original/preview_weather_app.png',
 		github: 'https://github.com/gizinski-jacek/weather-app-vue',
 		demo: 'https://weather-app-vue-sandy.vercel.app',
-	},
-	{
-		name: 'QR File Share',
-		thumbnail: './previews/thumbnail/preview_qr_file_share_sm.png',
-		image: './previews/original/preview_qr_file_share.png',
-		github: 'https://github.com/gizinski-jacek/qr-file-share',
-		demo: 'https://qr-file-share-umber.vercel.app',
 	},
 	{
 		name: 'TrackLimits',
@@ -48,18 +41,18 @@ export const projectList: {
 		demo: 'https://youtube-page.onrender.com',
 	},
 	{
+		name: 'Online Alarm Clock',
+		thumbnail: './previews/thumbnail/preview_online_alarm_clock_sm.png',
+		image: './previews/original/preview_online_alarm_clock.png',
+		github: 'https://github.com/gizinski-jacek/alarm-app',
+		demo: 'https://online-alarm-clock-35d8a.netlify.app',
+	},
+	{
 		name: 'Shopping Cart',
 		thumbnail: './previews/thumbnail/preview_shopping_cart_sm.jpg',
 		image: './previews/original/preview_shopping_cart.jpg',
 		github: 'https://github.com/gizinski-jacek/shopping-cart-react',
 		demo: 'https://shopping-cart-676k.onrender.com',
-	},
-	{
-		name: 'Blogster',
-		thumbnail: './previews/thumbnail/preview_blogster_sm.png',
-		image: './previews/original/preview_blogster.png',
-		github: 'https://github.com/gizinski-jacek/blog-API-MERN',
-		demo: 'https://blog-api-mern.onrender.com',
 	},
 	{
 		name: 'Where is Waldo',
@@ -74,13 +67,6 @@ export const projectList: {
 		image: './previews/original/preview_todo_list.jpg',
 		github: 'https://github.com/gizinski-jacek/ToDo-List-project',
 		demo: 'https://gizinski-jacek.github.io/ToDo-List-project',
-	},
-	{
-		name: 'Online Alarm Clock',
-		thumbnail: './previews/thumbnail/preview_online_alarm_clock_sm.png',
-		image: './previews/original/preview_online_alarm_clock.png',
-		github: 'https://github.com/gizinski-jacek/alarm-app',
-		demo: 'https://online-alarm-clock-35d8a.netlify.app',
 	},
 	{
 		name: 'Online Library',
@@ -116,5 +102,19 @@ export const projectList: {
 		image: './previews/original/preview_tnw_clone.jpg',
 		github: 'https://github.com/gizinski-jacek/responsive-design',
 		demo: 'https://responsive-design-xcjt.onrender.com',
+	},
+	{
+		name: 'Blogster',
+		thumbnail: './previews/thumbnail/preview_blogster_sm.png',
+		image: './previews/original/preview_blogster.png',
+		github: 'https://github.com/gizinski-jacek/blog-API-MERN',
+		demo: 'https://blog-api-mern.onrender.com',
+	},
+	{
+		name: 'CV Creator',
+		thumbnail: './previews/thumbnail/preview_cv_creator_sm.png',
+		image: './previews/original/preview_cv_creator.png',
+		github: 'https://github.com/gizinski-jacek/cv-project-react',
+		demo: 'https://cv-creator-c9c2ea.netlify.app',
 	},
 ];
